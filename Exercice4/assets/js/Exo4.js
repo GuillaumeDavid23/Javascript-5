625c9fd3fa57173edbee32ee959cccd8d39cb958
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-04 10:01:56 +0200

    Exo4

---
+++ Exercice4/assets/js/Exo4.js
@@ -0,0 +1,13 @@
+function changeImage(a) {
+    let element = document.getElementById("image");
+    element.src = "assets/img/logo.png";
+    
+}
+
+function bigImg(x) {
+    x.style.width = "300px";
+}
+  
+  function normalImg(x) {
+    x.style.width = "200px";
+}
